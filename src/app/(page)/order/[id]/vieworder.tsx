77a34c0af699ffc0f66ce3852f51e098b426378d
xa <87640952+xaywj@users.noreply.github.com>
2023-08-30 17:33:22 +0700

    design form view position order

--- src/app/(page)/order/[id]/vieworder.tsx
+++ src/app/(page)/order/[id]/vieworder.tsx
@@ -5,11 +5,12 @@ import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import BrowserUpdatedRoundedIcon from "@mui/icons-material/BrowserUpdatedRounded";
 import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded"; 
 import EditNoteRoundedIcon from "@mui/icons-material/EditNoteRounded";
-import LockResetRoundedIcon from "@mui/icons-material/LockResetRounded"; 
-
+import MultipleStopRoundedIcon from '@mui/icons-material/MultipleStopRounded';
 import Orderbalance from "./orderbalance";
 import Orderinfomation from "./orderinfomation";
 import Ordertransaction from "./ordertransaction";
+import ReceiptRoundedIcon from '@mui/icons-material/ReceiptRounded';
+import CurrencyExchangeRoundedIcon from '@mui/icons-material/CurrencyExchangeRounded';
 import Orderactivity from "./orderactivity";
 import Customerinfo from "./customerinfo";
 import Ordersummary from "./ordersummary";
@@ -17,12 +18,12 @@ export default function Vieworder() {
   const [status, setStatus] = useState(false);
   return (
     <div className="grid grid-cols-12 my-2 gap-5">
-      <div className="col-span-12 lg:col-span-9 flex  flex-wrap  items-center gap-2">
+      <div className="col-span-12 lg:col-span-7 flex  flex-wrap  items-center gap-2">
         <Link href="/order" className="bg-white rounded-lg p-2">
           <ArrowBackRoundedIcon fontSize="small" />
         </Link>
         <span className="font-bold text-xl">U-001</span>
-        <button className="btn btn-md btn-block-success">Active</button>
+        <button className="btn btn-md btn-block-warning">Pending</button>
         <button className="btn btn-md btn-block-error">
           <DeleteForeverRoundedIcon fontSize="small" /> Delete
         </button>
@@ -33,9 +34,15 @@ export default function Vieworder() {
           <BrowserUpdatedRoundedIcon fontSize="small" /> Download
         </button>
       </div>
-      <div className="col-span-12 lg:col-span-3 lg:text-end">
+      <div className="col-span-12 lg:col-span-5 flex flex-wrap gap-2 lg:text-end">
         <button className="btn btn-md btn-default">
-          <LockResetRoundedIcon fontSize="small" /> Resetpassword
+          <ReceiptRoundedIcon fontSize="small" /> Settlement
+        </button>
+        <button className="btn btn-md btn-default">
+          <CurrencyExchangeRoundedIcon fontSize="small" /> Cut Position
+        </button>
+        <button className="btn btn-md btn-default">
+          <MultipleStopRoundedIcon fontSize="small" /> Transfer
         </button>
       </div>
 
