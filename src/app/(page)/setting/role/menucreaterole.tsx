03418676f4a49f8eb897a8ecf6025041c28e3824
xa <87640952+xaywj@users.noreply.github.com>
2023-09-13 14:47:10 +0700

    add attrb for form role

--- src/app/(page)/setting/role/menucreaterole.tsx
+++ src/app/(page)/setting/role/menucreaterole.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Iconadd from "@/icon/iconadd";
 import Iconclose from "@/icon/iconclose";
-import Iconsave from "@/icon/iconsave"; 
+import Iconsave from "@/icon/iconsave";
 import React, { useState } from "react";
 
 export default function Menucreaterole() {
@@ -9,6 +9,22 @@ export default function Menucreaterole() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const [formdata, setFormdata] = useState({
+    name: "",
+    description: ""
+  });
+
+  const handleForm = (e: any) => {
+    const { name, value } = e.target;
+    setFormdata({ ...formdata, [name]: value });
+  }
+
+  const handlesubmit = (e: any) => {
+    console.log(formdata);
+    e.preventDefault();
+    handleClose();
+  }
 
   return (
     <>
@@ -21,7 +37,7 @@ export default function Menucreaterole() {
         <Iconadd /> Create New Role
       </button>
 
-      <div className={`modal ${open?'show':'hide'}`}>
+      <div className={`modal ${open ? 'show' : 'hide'}`}>
         <div className="modal-content  modal-sm">
           <div className="modal-header flex justify-between items-center py-5">
             <h5 className="modal-title">Create New Role</h5>
@@ -34,27 +50,29 @@ export default function Menucreaterole() {
             </button>
           </div>
           <div className="modal-body">
-            <div className="grid grid-cols-12 gap-2">
-              <div className="col-span-12">
-                <b>Role Name</b>
-                <input type="text" name="name" className="form-control" />
-              </div>
+            <form method="post" onSubmit={handlesubmit}>
+              <div className="grid grid-cols-12 gap-2">
+                <div className="col-span-12">
+                  <b>Role Name</b>
+                  <input type="text" required name="name" value={formdata.name} onChange={handleForm} className="form-control" />
+                </div>
 
-              <div className="col-span-12">
-                <b>Description</b>
-                <textarea name="description" id="" cols={30} rows={3} className="form-control"></textarea>
-              </div>
+                <div className="col-span-12">
+                  <b>Description</b>
+                  <textarea name="description" value={formdata.description} onChange={handleForm} id="" cols={30} rows={3} className="form-control"></textarea>
+                </div>
 
-              <div className="col-span-12 mt-3">
-                <button
-                  onClick={handleClose}
-                  className="btn btn-secondary w-full"
-                >
-                  <Iconsave />
-                  Create
-                </button>
+                <div className="col-span-12 mt-3">
+                  <button
+                    type="submit"
+                    className="btn btn-secondary w-full"
+                  >
+                    <Iconsave />
+                    Create
+                  </button>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- src/app/(page)/setting/user/createuser.tsx
+++ src/app/(page)/setting/user/createuser.tsx
@@ -3,16 +3,37 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Switch from "@/components/tools/switch/switch";
 import Iconclose from "@/icon/iconclose";
-import Icondownloadoutline from "@/icon/icondownloadoutline";
 import Iconsave from "@/icon/iconsave";
 import Iconadd from "@/icon/iconadd";
 
 export default function Menucreateuser() {
   const [open, setOpen] = useState(false);
-  const [status, setStatus] = useState(false);
   const handleClose = () => {
     setOpen(false);
   };
+
+
+  const [formdata, setFormdata] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    codecountry: "",
+    role_id: 0,
+    status: false,
+    profile: "",
+  });
+
+  const handleForm = (e: any) => {
+    const { name, value } = e.target;
+    setFormdata({ ...formdata, [name]: value });
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    handleClose();
+    console.log(formdata);
+  }
+
   return (
     <>
       <button
@@ -23,7 +44,7 @@ export default function Menucreateuser() {
         <Iconadd /> Create New User
       </button>
 
-      <div className={`modal ${open?"show":'hide'}`}>
+      <div className={`modal ${open ? "show" : 'hide'}`}>
         <div className="modal-content modal-sm">
           <div className="modal-header flex justify-between items-center py-5">
             <h5 className="modal-title">Update User</h5>
@@ -36,62 +57,79 @@ export default function Menucreateuser() {
             </button>
           </div>
           <div className="modal-body">
-            <div className="grid grid-cols-12 gap-2">
-              <div className="col-span-12 flex items-center gap-3">
-                <Image
-                  src="/avatar.jpg"
-                  width="100"
-                  height="100"
-                  alt="AAA"
-                  className="rounded shadow"
-                />
-                <button className="btn btn-default">
-                  <Icondownloadoutline />
-                  Upload
-                </button>
-              </div>
-              <div className="col-span-12">
-                <b>Username</b>
-                <input type="text" className="form-control" />
-              </div>
+            <form method="post" onSubmit={handleSubmit}>
+              <div className="grid grid-cols-12 gap-2">
+                <div className="col-span-12 flex items-center gap-3">
+                  <Image
+                    src={formdata.profile || '/avatar.jpg'}
+                    width="100"
+                    height="100"
+                    alt="profile"
+                    className="rounded shadow"
+                  />
+                  <input required type="file" name="profile" className="form-control"
+                    onChange={(e: any) => {
+                      const file = e.target.files[0];
+                      if (!file) return;
+                      const reader = new FileReader();
+                      reader.onloadend = () => {
+                        setFormdata({ ...formdata, profile: reader.result as string });
+                      }
+                      reader.readAsDataURL(file);
+                    }}
+                  />
+                </div>
+                <div className="col-span-12">
+                  <b>Username</b>
+                  <input required type="text" value={formdata.username} name="username" className="form-control" onChange={handleForm} />
+                </div>
 
-              <div className="col-span-12">
-                <b>Phone Number</b>
+                <div className="col-span-12">
+                  <b>Phone Number</b>
+                </div>
+                <div className="col-span-5">
+                  <select required value={formdata.codecountry} name="codecountry" className="form-select" onChange={handleForm}  >
+                    <option value="856">+856</option>
+                    <option value="63">+63</option>
+                    <option value="64">+64</option>
+                  </select>
+                </div>
+                <div className="col-span-7">
+                  <input required type="text" name="phone" value={formdata.phone} className="form-control" onChange={handleForm} />
+                </div>
+                <div className="col-span-12">
+                  <b>Email</b>
+                  <input type="text" name="email" required value={formdata.email} className="form-control" onChange={handleForm} />
+                </div>
+                <div className="col-span-12">
+                  <b>Role</b>
+                  <select required value={formdata.role_id} name="role_id" className="form-select" onChange={handleForm} >
+                    <option value="admin">Admin</option>
+                    <option value="user">User</option>
+                    <option value="guest">Guest</option>
+                  </select>
+                </div>
+                <div className="col-span-12">
+                  <b>Status</b>
+                  <br />
+                  <Switch
+                    checked={formdata.status}
+                    onClick={() => setFormdata({ ...formdata, status: !formdata.status })}
+                    name="status"
+                    color="success"
+                  />
+                </div>
+                <div className="col-span-12 mt-3">
+                  <button
+                    type="submit"
+                    className="btn btn-secondary w-full"
+                  >
+                    <Iconsave />
+                    Save
+                  </button>
+                </div>
               </div>
-              <div className="col-span-5">
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-span-7">
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-span-12">
-                <b>Email</b>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-span-12">
-                <b>Role</b>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-span-12">
-                <b>Status</b>
-                <br />
-                <Switch
-                  checked={status}
-                  onChange={() => setStatus(!status)}
-                  name="status"
-                  color="success"
-                />
-              </div>
-              <div className="col-span-12 mt-3">
-                <button
-                  onClick={handleClose}
-                  className="btn btn-secondary w-full"
-                >
-                  <Iconsave />
-                  Save
-                </button>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
